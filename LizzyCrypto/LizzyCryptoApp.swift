cdc427dc03a8ae14457e02d9e22b368f22e52dfb
cschiappa <33517984+cschiappa@users.noreply.github.com>
2024-04-24 18:30:04 +0100

    Initial Commit

---
+++ LizzyCrypto/LizzyCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LizzyCryptoApp.swift
+//  LizzyCrypto
+//
+//  Created by Cristina on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct LizzyCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
